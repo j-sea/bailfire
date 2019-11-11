c3eb6438225cd3925f227420fb0ea56d1bd0e898
JandyStephens <36689860+JandyStephens@users.noreply.github.com>
2019-11-11 12:30:30 -0800

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -41,7 +41,7 @@ module.exports = function (email, inviteUUID) {
          html: "<h1>Someone has sent you a ScATTeR group invite!</h1> \
                <p>If you don't know who sent this, or you aren't expecting an invite, <b>please</b> ignore this e-mail.</p> \
                <p>ScATTeR is a Group Locating app where you can keep your group outings coordinated between members.</p> \
-               <p>Your location is shared to your group automatically when you want.</p> \
+               <p>Your location is shared to your group automatically, when you want.</p> \
                <p>If you are sure you want to join this person's ScATTeR group and share your location, click here to <b>accept the invite</b>: <a href=\"" + urlPrepend + "/group-invite/accept/" + inviteUUID + "\">" + urlPrepend + "/group-invite/accept/" + inviteUUID + "</a></p> \
                <p>If you don't want to join this person's ScATTeR group, either ignore this e-mail or click here to <b>reject the invite</b>: <a href=\"" + urlPrepend + "/group-invite/reject/" + inviteUUID + "\">" + urlPrepend + "/group-invite/reject/" + inviteUUID + "</a></p> \
                <p>If you'd like to learn more about ScATTeR, click here: <a href=\"https://scatter-web.herokuapp.com/\">https://scatter-web.herokuapp.com/</a></p>"
