c9729810c62194a521afe6fd4b5e5ed74b067f1a
Jandy <reverdy@protonmail.com>
2019-11-11 18:27:41 -0800

    updated view specific group route to return group with user details

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Import necessary Node.js files
 const bcrypt = require('bcrypt');
 const db = require('../../models');
-const {Op} = require('sequelize');
+const { Op } = require('sequelize');
 
 // Create an Express Router to allow routing via files external to server.js
 const router = require('express').Router();
@@ -25,7 +25,7 @@ router.post("/auth/login", function (req, res) {
 	if (!Object.prototype.hasOwnProperty.call(req.body, 'password') || typeof req.body.password !== 'string') {
 		req.body.password = '';
 	}
-	
+
 	if (req.body.password !== '' && (req.body.email !== '' || req.body.phone !== '')) {
 		db.Users.findOne({
 			where: {
@@ -39,29 +39,30 @@ router.post("/auth/login", function (req, res) {
 				],
 			},
 		})
-		.then(function (dbUser) {
-			if (!dbUser) {
-				res.status(401).send('Login credentials incorrect!');
-			}
-			else {
-				if (bcrypt.compareSync(req.body.password, dbUser.password)) {
-					//create new session property "user", set equal to logged in user
-					req.session.user = { id: dbUser.id, email: dbUser.email, phone: dbUser.phone, user_uuid: dbUser.user_uuid }
-					req.session.error = null;
-					res.status(200).json(req.session);
+			.then(function (dbUser) {
+				if (!dbUser) {
+					res.status(401).send('Login credentials incorrect!');
+					alert('No user found with those credentials. Please create an account.')
 				}
 				else {
-					//delete existing user, add error
-					req.session.user = false;
-					req.session.error = 'Login credentials incorrect!';
-					res.status(401).send("Login credentials incorrect!");
+					if (bcrypt.compareSync(req.body.password, dbUser.password)) {
+						//create new session property "user", set equal to logged in user
+						req.session.user = { id: dbUser.id, email: dbUser.email, phone: dbUser.phone, user_uuid: dbUser.user_uuid }
+						req.session.error = null;
+						res.status(200).json(req.session);
+					}
+					else {
+						//delete existing user, add error
+						req.session.user = false;
+						req.session.error = 'Login credentials incorrect!';
+						res.status(401).send("Login credentials incorrect!");
+					}
 				}
-			}
-		})
-		.catch(function (error) {
-			console.log(error);
-			res.status(500).send('Encountered server error when logging in!');
-		});
+			})
+			.catch(function (error) {
+				console.log(error);
+				res.status(500).send('Encountered server error when logging in!');
+			});
 	}
 	else {
 		//delete existing user, add error
